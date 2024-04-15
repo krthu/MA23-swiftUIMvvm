f4a3e26295c74e28bb2d1e145dd3c1d19db6853e
KT <thun.kristian@gmail.com>
2024-04-15 12:22:36 +0200

    first init

---
+++ MA23_MVVM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MA23_MVVM
+//
+//  Created by Kristian Thun on 2024-04-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MA23_MVVM/MA23_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MA23_MVVMApp.swift
+//  MA23_MVVM
+//
+//  Created by Kristian Thun on 2024-04-15.
+//
+
+import SwiftUI
+
+@main
+struct MA23_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
