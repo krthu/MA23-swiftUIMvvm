5ab31bc4eda1a0adf08869e8bd1ceeeb63a07c7a
KT <thun.kristian@gmail.com>
2024-04-16 09:01:24 +0200

    start of tab done

--- MA23_MVVM/UserListView.swift
+++ MA23_MVVM/UserListView.swift
@@ -8,8 +8,79 @@
 import SwiftUI
 
 struct UserListView: View {
+    @StateObject var usersViewModel = UsersViewModel()
+    @State var addSheetIsShown = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack {
+                List(){
+                    ForEach(usersViewModel.users){ user in
+                        NavigationLink(destination: UserDetailView()){
+                            userRow(user: user)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Users")
+            .navigationBarItems(trailing: Button(action: {
+                addSheetIsShown = true
+            }, label: {
+                Image(systemName: "plus")
+                    .foregroundColor(.green)
+            })
+            .sheet(isPresented: $addSheetIsShown, content: {
+                addProfileSheet(usersViewModel : usersViewModel, addSheetIsShowen: $addSheetIsShown)
+            })
+            )
+        }
+    }
+}
+
+struct userRow: View {
+    var user: User
+    
+    var body: some View {
+        HStack{
+            Image(systemName: user.profileImage)
+            VStack(alignment: .leading){
+                Text(user.name)
+                Text(user.email)
+            }
+        }
+    }
+}
+
+struct addProfileSheet: View{
+    @ObservedObject var usersViewModel: UsersViewModel
+    @State var name : String = ""
+    @State var email : String = ""
+    @Binding var addSheetIsShowen: Bool
+    
+    var body: some View{
+        NavigationStack{
+            List{
+                HStack{
+                    Text("Name:")
+                    TextField("John Doe", text: $name)
+                    
+                        
+                }
+                
+                HStack{
+                    Text("E-mail")
+                    TextField("john.doh@gmail.com", text: $email)
+                }
+            }
+            .navigationTitle("Add user")
+            Button(action: {
+                usersViewModel.users.append(User(name: name, email: email, profileImage: "brain.filled.head.profile"))
+                addSheetIsShowen = false
+                
+            }, label: {
+                Text("Save")
+            })
+        }
     }
 }
 
